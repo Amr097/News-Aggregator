9896b4618052fb2ef7f12a53395bf7b4d9d993a9
Amr <amruser97@gmail.com>
2025-08-02 16:57:46 +0300

    normalized news posts

---
+++ src/helpers/normalizeNewsPost.js
@@ -0,0 +1,40 @@
+const normalizeNewsArticle = (article) => {
+  // Check if it's a Guardian article (has webTitle)
+  if (article.webTitle) {
+    return {
+      id: article.id,
+      title: article.webTitle,
+      image: null,
+      sourceName: "The Guardian",
+      publishedAt: article.webPublicationDate,
+      type: article.type,
+      content: null,
+      url: article.webUrl,
+    };
+  }
+
+  return {
+    id: generateId(article.url),
+    title: article.title,
+    image: article.urlToImage,
+    sourceName: article.source.name,
+    publishedAt: article.publishedAt,
+    type: "article",
+    content: article.content,
+    url: article.url,
+  };
+};
+
+// Generate simple ID from URL
+function generateId(url) {
+  return (
+    url
+      .split("/")
+      .pop()
+      .replace(/\.(html|php)$/, "") || Math.random().toString(36)
+  );
+}
+
+export function normalizeArticles(articles) {
+  return articles.map((article) => normalizeNewsArticle(article));
+}
--- src/services/fetchAllNews.js
+++ src/services/fetchAllNews.js
@@ -1,13 +1,14 @@
 import fetchTheGaurdianApi from "./fetchTheGaurdianApi";
 import fetchNewsApi from "./fetchNewsApi";
+import { normalizeArticles } from "@/helpers/normalizeNewsPost";
 
 export const fetchAllNews = async () => {
   const gaurdian = await fetchTheGaurdianApi("/search").then(
     (res) => res.data.response.results
   );
-  const news = await fetchNewsApi("/top-headlines", { country: "eg" }).then(
+  const news = await fetchNewsApi("/top-headlines", { country: "us" }).then(
     (res) => res.data.articles
   );
-
-  return [...gaurdian, ...news];
+  console.log([...news, ...gaurdian]);
+  return normalizeArticles([...news, ...gaurdian]);
 };
