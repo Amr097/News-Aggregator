e59b6fed434d5fb346f9b808170f62e399ffb703
Amr <amruser97@gmail.com>
2025-08-03 13:45:02 +0300

    set search query params via input

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,25 +3,56 @@ import "../../app/styles/search.css";
 
 import { Input } from "../ui/input";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useSearchParams } from "next/navigation";
 
 import { useDebounce } from "use-debounce";
 import { useQueryClient } from "@tanstack/react-query";
 
+import { formUrlQuery, removeKeysFromQuery } from "@/helpers/url";
+
 const Search = () => {
+  const searchParams = useSearchParams();
   const queryClient = useQueryClient();
 
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState(searchParams.get("q") || "");
   const [debouncedSearchQuery] = useDebounce(searchQuery, 250);
+  const hasUserSearched = useRef(false);
 
   useEffect(() => {
-    queryClient.invalidateQueries({
-      queryKey: ["allNews"],
-    });
-  }, [debouncedSearchQuery, queryClient]);
+    const params = new URLSearchParams(searchParams.toString());
+    const query = params.get("q");
+    let url = window.location.toString();
+
+    // Skip if user hasn't searched yet
+    if (!hasUserSearched.current && !query) return;
+
+    if (debouncedSearchQuery)
+      url = formUrlQuery({
+        params: searchParams.toString(),
+        key: "q",
+        value: debouncedSearchQuery,
+      });
+    else
+      url = removeKeysFromQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["q"],
+      });
+
+    if (url !== window.location.toString())
+      window.history.replaceState(null, "", url);
+
+    setTimeout(() => {
+      queryClient.invalidateQueries({
+        queryKey: ["allNews"],
+        exact: true,
+      });
+    }, 0);
+  }, [debouncedSearchQuery]);
 
   const handleInputChange = (e) => {
     setSearchQuery(e.target.value);
+    hasUserSearched.current = true;
   };
 
   return (
---
+++ src/helpers/url.js
@@ -0,0 +1,28 @@
+import queryString from "query-string";
+
+export const formUrlQuery = ({ params, key, value }) => {
+  const qs = queryString.parse(params);
+
+  qs[key] = value;
+
+  return queryString.stringifyUrl({
+    url: window.location.pathname,
+    query: qs,
+  });
+};
+
+export const removeKeysFromQuery = ({ params, keysToRemove }) => {
+  const qs = queryString.parse(params);
+
+  keysToRemove.forEach((key) => {
+    delete qs[key];
+  });
+
+  return queryString.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: qs,
+    },
+    { skipNull: true }
+  );
+};
