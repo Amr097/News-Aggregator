d275c07a007bb750f6e8b06ef80b9be9c9c4c8c6
Amr <amruser97@gmail.com>
2025-08-04 02:34:13 +0300

    fix loading bug where search bar was re-mounted on loading shifts

--- src/features/home/components/newsGrid.js
+++ src/features/home/components/newsGrid.js
@@ -5,19 +5,19 @@ import { useQuery } from "@tanstack/react-query";
 import fetchNextServer from "@/services/fetchNextServer";
 import { useSearchParams } from "next/navigation";
 import EmptyState from "@/components/partials/EmptyState";
+import GridSkeleton from "./GridSkeleton";
 
 export default function NewsGrid() {
   const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams.toString());
   const query = params.get("q");
 
-  const { data: newsItems } = useQuery({
+  const { data: newsItems, isLoading } = useQuery({
     queryKey: ["allNews", query],
     queryFn: () =>
       fetchNextServer(`/api/news`, {
         searchQuery: query,
       }),
-    suspense: true,
     throwOnError: true,
   });
 
@@ -25,7 +25,9 @@ export default function NewsGrid() {
     return <EmptyState query={query} />;
   }
 
-  return (
+  return isLoading ? (
+    <GridSkeleton />
+  ) : (
     <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 px-4 sm:px-8 md:px-12 lg:px-16 xl:px-24 py-6 sm:py-8">
       {newsItems &&
         newsItems.length > 0 &&
