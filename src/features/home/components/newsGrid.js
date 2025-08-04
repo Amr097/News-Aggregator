ab1369ff6f155bc290de1f347a534d2e6220cc12
Amr <amruser97@gmail.com>
2025-08-04 16:45:10 +0300

    Revert "added gsap scroll animation"
    This reverts commit 3b83882019088c5de3dfb451597385cee2c4f1d2.

--- src/features/home/components/newsGrid.js
+++ src/features/home/components/newsGrid.js
@@ -6,8 +6,6 @@ import fetchNextServer from "@/services/fetchNextServer";
 import { useSearchParams } from "next/navigation";
 import EmptyState from "@/components/partials/EmptyState";
 import GridSkeleton from "./GridSkeleton";
-import { useEffect } from "react";
-import { useScrollAnimation } from "@/hooks/useScrollAnimation";
 
 export default function NewsGrid() {
   const searchParams = useSearchParams();
@@ -23,12 +21,6 @@ export default function NewsGrid() {
     throwOnError: true,
   });
 
-  const { addToRefs, clearRefs } = useScrollAnimation([newsItems]);
-
-  useEffect(() => {
-    clearRefs();
-  }, [query, clearRefs]);
-
   if (newsItems && newsItems.length === 0) {
     return <EmptyState query={query} />;
   }
@@ -39,15 +31,7 @@ export default function NewsGrid() {
     <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 px-4 sm:px-8 md:px-12 lg:px-16 xl:px-24 py-6 sm:py-8">
       {newsItems &&
         newsItems.length > 0 &&
-        newsItems.map((item) => (
-          <li
-            key={item.id}
-            ref={addToRefs}
-            style={{ transform: "translateY(30px)", opacity: 0 }}
-          >
-            <NewsCard newsData={item} />
-          </li>
-        ))}
+        newsItems.map((item) => <NewsCard key={item.id} newsData={item} />)}
     </ul>
   );
 }
